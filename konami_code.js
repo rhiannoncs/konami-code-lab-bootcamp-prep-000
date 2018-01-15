57fd606ae489ee47e71086fda7fff2a9f16005c2
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-15 02:05:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,21 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0;
-  const input = document.querySelector('input')
+  const input = document.querySelector('input');
   
-  input.addEventListener('keydown', function(e) 
-  
-
+  input.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+    
+    if (key === code[index]) {
+      index ++;
+      
+      if (index === code.length) {
+        alert("Congratulations!");
+        
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
 }
